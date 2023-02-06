441add742f2c2634fd9b16d64a0a810c0768f40e
albin5551 <albin551@hotmail.com>
2023-02-06 19:10:22 +0530

    trhome

--- libraryfr/src/app/login/trhome/trhome.component.ts
+++ libraryfr/src/app/login/trhome/trhome.component.ts
@@ -7,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TrhomeComponent implements OnInit {
 
+  showNavigationIndicators = false;
+	images = [944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/900/500`);
   constructor() { }
 
   ngOnInit(): void {
+
   }
+
+  
 
 }
